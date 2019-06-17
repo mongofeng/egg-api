43a1de436a7d5e49e0f6cbdf5738449f3451dbfa
yangpu <yangpu@xlink.cn>
2019-06-17 13:09:41 +0800

    feat(课程搜索): 添加课程搜索和获取openid

--- app/schedule/debug.js
+++ app/schedule/debug.js
@@ -5,7 +5,7 @@ class UpdateCache extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      disable: true, // 配置该参数为 true 时，这个定时任务不会被启动。
+      // disable: true, // 配置该参数为 true 时，这个定时任务不会被启动。
       interval: '1m', // 1 分钟间隔
       type: 'worker', // 指定所有的 worker 都需要执行
     };
@@ -14,36 +14,10 @@ class UpdateCache extends Subscription {
   // subscribe 是真正定时任务执行时被运行的函数
   async subscribe() {
     const { ctx } = this;
-    const data = {
-      touser: 'oVB5OwyVDKfTZq4T61_p2roSg1tA',
-      template_id: 'arueuQozNMNHBZ7BQ5hsCzxxC9X38tJ1Kmu7Oa1JjEY',
-      url: 'http://weixin.qq.com/download',
-      data: {
-        first: {
-          value: '微信消息推送',
-          color: '#173177',
-        },
-        keyword1: {
-          value: '杨璞',
-          color: '#1d1d1d',
-        },
-        keyword2: {
-          value: '男',
-          color: '#1d1d1d',
-        },
-        keyword3: {
-          value: '23',
-          color: '#1d1d1d',
-        },
-        remark: {
-          value: '已登记！',
-          color: '#173177',
-        },
-      },
-    };
-
-    const { access_token } = await ctx.service.wechat.wechatToken();
-    await ctx.service.wechat.sendTemplateMsg({ access_token, data });
+    const list = await ctx.service.schedule.fetchCurrentCourse();
+    console.log(list);
+    // const { access_token } = await ctx.service.wechat.wechatToken();
+    // await ctx.service.wechat.sendTemplateMsg({ access_token, data });
     console.log('定时任务执行');
   }
 }
--- app/service/schedule.js
+++ app/service/schedule.js
@@ -13,19 +13,73 @@ class ScheduleService extends Service {
   }
 
   // 上课提醒
-  async sendCourse() {
+  async fetchCurrentCourse() {
     const { ctx } = this;
-    const data = await ctx.model.Course.find({
-      startDate: {
-        $lte: new Date().getTime(),
+    const nowDate = new Date();
+    const data = await ctx.model.Course.aggregate([{
+      $match: {
+        startDate: {
+          $lte: nowDate.getTime(),
+        },
+        endDate: {
+          $gte: nowDate.getTime(),
+        },
+        // day: nowDate.getDay(),
       },
-      endDate: {
-        $gte: new Date().getTime(),
+    }, {
+      $unwind: '$studentIds', // 结构数组
+    }, {
+      $lookup:
+        {
+          from: 'student',
+          let: { stuId: { $toObjectId: '$studentIds' } }, // 把studentid变成Object(id)
+          pipeline: [
+            {
+              $match:
+              {
+                $expr: // 接受聚合表达式
+                  { $eq: [ '$_id', '$$stuId' ] },
+              },
+            },
+            {
+              $project: {
+                openId: '$openId',
+                stu_name: '$name',
+                stu_id: '$_id',
+              },
+            },
+          ],
+          as: 'student',
+        },
+    }, {
+      $replaceRoot: {
+        newRoot: {
+          $mergeObjects: [
+            { $arrayElemAt: [ '$student', 0 ] }, // 拿数组的第一位，变成一个object
+            '$$ROOT', // 覆盖数组
+          ],
+        },
       },
-      day: new Date().getDay(),
-    });
-    console.log(data);
+    }, {
+      $project: { // 只保留openid,和课程id
+        openId: 1,
+        stu_name: 1,
+        name: 1,
+        stu_id: 1,
+      },
+    }, {
+      $match: {
+        $expr: { $ne: [ '$openId', '' ] }, // 去除openid为空的状况
+      },
+    }]);
+    return data;
   }
 }
 
 module.exports = ScheduleService;
+/**
+ * match是搜索条件
+ * unwind是把数组变成一个条条数据
+ * project是更改/保留/去除/字段
+ * lookup是链表查询
+ */
