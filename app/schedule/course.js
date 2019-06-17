c56e0ce3a284e1308136d8c25cb126d8707c8c3f
yangpu <yangpu@xlink.cn>
2019-06-17 14:14:46 +0800

    feat(定时任务): 调整定时任务为数据库的数据

--- app/schedule/course.js
+++ app/schedule/course.js
@@ -5,7 +5,8 @@ class CourseSchedule extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      cron: '0 0 10 * * *', // 10.0分执行一次
+      // cron: '0 0 10 * * *', // 10.0分执行一次
+      cron: '0 30 8 * * *', // 8.30分执行一次
       type: 'worker', // 每台机器上只有一个 worker 会执行这个定时任务，每次执行定时任务的 worker 的选择是随机的
     };
   }
@@ -14,65 +15,36 @@ class CourseSchedule extends Subscription {
   async subscribe() {
     const { ctx } = this;
 
-    const person = [
-      'oVB5Ow4k7ktizMVifdH5zjVT9EQ4', // 林兄
-      'oVB5OwyVDKfTZq4T61_p2roSg1tA', // ziji
-    ];
-
-    const list = person.map(item => {
+    const list = await ctx.service.schedule.fetchCurrentCourse();
+    console.log(list);
+    const api = list.map(item => {
       return {
-        touser: item,
+        touser: item.openId,
         template_id: 'LGHVsyKEBAsk79EDA2surQhWVSGl-oSq-MtPJ4fe1r0',
         url: 'http://47.107.144.222/platform',
         data: {
           course: {
-            value: '素描课程',
+            value: item.name,
             color: '#173177',
           },
           time: {
-            value: '14:30',
+            value: item.time,
             color: '#1d1d1d',
           },
           teacher: {
-            value: '小羊老师',
+            value: item.teacher_name,
             color: '#1d1d1d',
           },
           remark: {
-            value: '记得上课哦，不要迟到的啦！每天9点准点提醒hhhhh',
+            value: item.desc,
             color: '#173177',
           },
         },
       };
     });
 
-
-    // {
-    //   "touser": "oVB5OwyVDKfTZq4T61_p2roSg1tA",
-    //   "template_id": "LGHVsyKEBAsk79EDA2surQhWVSGl-oSq-MtPJ4fe1r0",
-    //   "url": "http://47.107.144.222/platform",
-    //   "data": {
-    //     "course": {
-    //       "value": "素描课程",
-    //       "color": "#173177"
-    //     },
-    //     "time": {
-    //       "value": "14:30",
-    //       "color": "#1d1d1d"
-    //     },
-    //     "teacher": {
-    //       "value": "小羊老师",
-    //       "color": "#1d1d1d"
-    //     },
-    //     "remark": {
-    //       "value": "记得上课哦，不要迟到的啦！",
-    //       "color": "#173177"
-    //     }
-    //   }
-    // }
-
-
     const { access_token } = await ctx.service.wechat.wechatToken();
-    list.forEach(data => {
+    api.forEach(data => {
       ctx.service.wechat.sendTemplateMsg({ access_token, data });
     });
     console.log('定时任务执行');
--- app/schedule/debug.js
+++ app/schedule/debug.js
@@ -5,7 +5,7 @@ class UpdateCache extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      // disable: true, // 配置该参数为 true 时，这个定时任务不会被启动。
+      disable: true, // 配置该参数为 true 时，这个定时任务不会被启动。
       interval: '1m', // 1 分钟间隔
       type: 'worker', // 指定所有的 worker 都需要执行
     };
@@ -16,9 +16,38 @@ class UpdateCache extends Subscription {
     const { ctx } = this;
     const list = await ctx.service.schedule.fetchCurrentCourse();
     console.log(list);
-    // const { access_token } = await ctx.service.wechat.wechatToken();
-    // await ctx.service.wechat.sendTemplateMsg({ access_token, data });
+    const api = list.map(item => {
+      return {
+        touser: item.openId,
+        template_id: 'LGHVsyKEBAsk79EDA2surQhWVSGl-oSq-MtPJ4fe1r0',
+        url: 'http://47.107.144.222/platform',
+        data: {
+          course: {
+            value: item.name,
+            color: '#173177',
+          },
+          time: {
+            value: item.time,
+            color: '#1d1d1d',
+          },
+          teacher: {
+            value: item.teacher_name,
+            color: '#1d1d1d',
+          },
+          remark: {
+            value: item.desc,
+            color: '#173177',
+          },
+        },
+      };
+    });
+
+    const { access_token } = await ctx.service.wechat.wechatToken();
+    api.forEach(data => {
+      ctx.service.wechat.sendTemplateMsg({ access_token, data });
+    });
     console.log('定时任务执行');
+    console.log('wechat 定时任务执行');
   }
 }
 
