1a936a0439d16ac050919f72ff0b084cf62bba86
yangpu <18312701884@163.com>
2019-07-14 16:16:51 +0800

    fix:docker配置

--- config/config.docker.js
+++ config/config.docker.js
@@ -12,6 +12,16 @@ module.exports = appInfo => {
       url: 'mongodb://database:27017/education',
       options: {},
     },
+
+    // 微信的配置
+    wechat: {
+      appid: 'wx9bb9b35bb6d4f980',
+      secret: '9a1b5839cba147e16d088ec98853951e',
+      token: 'wechat', // 消息服务号需要的token
+      grant_type: 'client_credential', // 默认
+      defalult_url: 'https://api.weixin.qq.com/',
+      mp_url: 'https://mp.weixin.qq.com/',
+    },
   };
 
   return config;
