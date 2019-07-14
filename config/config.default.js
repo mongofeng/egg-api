d5f9e1448e10696ad875f2f2bb044df2f381d6db
yangpu <18312701884@163.com>
2019-07-14 18:34:40 +0800

    feat:添加配置

--- config/config.default.js
+++ config/config.default.js
@@ -69,8 +69,10 @@ module.exports = appInfo => {
 
     // 微信的配置
     wechat: {
-      appid: 'wx6bce565776a81ced',
-      secret: 'b2b49cfc626b99a42536837928a32893',
+      // appid: 'wx6bce565776a81ced',
+      // secret: 'b2b49cfc626b99a42536837928a32893',
+      appid: 'wx9bb9b35bb6d4f980',
+      secret: '9a1b5839cba147e16d088ec98853951e',
       token: 'wechat', // 消息服务号需要的token
       grant_type: 'client_credential', // 默认
       defalult_url: 'https://api.weixin.qq.com/',
