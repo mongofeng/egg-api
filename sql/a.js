c5188585f3bc342afe1238af3d7b19f00beb564d
yangpu <yangpu@xiao100.com>
2020-12-04 10:29:39 +0800

    feat: 添加统计文档

--- sql/a.js
+++ sql/a.js
@@ -1,10 +1,20 @@
-var obj = {$group:{_id:'$studentIds',amount:{$sum:'$amount'},count:{$sum:'$count'},used:{$sum:'$used'},surplus:{$sum:'$surplus'},overdueCount:{$sum:{$cond:['$beOverdue','$surplus',0]}},activiteCount:{$sum:{$cond:['$isActive','$count',0]}},unActiviteCount:{$sum:{$cond:['$isActive',0,'$count']}}}}
 
 
-console.log(obj)
 
 
-var lookup = {$lookup:{from:'student',let:{stuId:{$toObjectId:'$_id'}},pipeline:[{$match:{$expr:{$eq:['$_id','$$stuId']}}}],as:'student'}}
+// 到期
+var tuisong = [
+  {$unwind:'$studentIds'},
+  {$group:{_id:'$studentIds', n: {$sum: 1 },amount:{$sum:'$amount'},count:{$sum:'$count'},used:{$sum:'$used'},surplus:{$sum:'$surplus'},overdueCount:{$sum:{$cond:['$beOverdue','$surplus',0]}},activiteCount:{$sum:{$cond:['$isActive','$count',0]}},unActiviteCount:{$sum:{$cond:['$isActive',0,'$count']}}}},
+  {$lookup:{from:'student',let:{stuId:{$toObjectId:'$_id'}},pipeline:[{$match:{$expr:{$eq:['$_id','$$stuId']}}}],as:'student'}},
+  {$match: { 'student.status': 1,  surplus: {$lte: 6}}},
+  { $sort : { surplus : 1, count: -1 } }
+]
 
-
-var a = {$project:{_id:1,student:{$arrayElemAt:['$student',0]},amount:1,count:1,used:1,surplus:1,overdueCount:1,activiteCount:1,unActiviteCount:1}}
+// 统计
+var total  = [
+  {$unwind:'$studentIds'},
+  {$group:{_id:'$studentIds', n: {$sum: 1 },amount:{$sum:'$amount'},count:{$sum:'$count'},used:{$sum:'$used'},surplus:{$sum:'$surplus'},overdueCount:{$sum:{$cond:['$beOverdue','$surplus',0]}},activiteCount:{$sum:{$cond:['$isActive','$count',0]}},unActiviteCount:{$sum:{$cond:['$isActive',0,'$count']}}}},
+  {$lookup:{from:'student',let:{stuId:{$toObjectId:'$_id'}},pipeline:[{$match:{$expr:{$eq:['$_id','$$stuId']}}}],as:'student'}},
+  { $sort : { surplus : 1, count: -1 } }
+]
