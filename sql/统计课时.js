b5509a47f610446e657e1e7e555630a3ebe8c784
yangpu <yangpu@xiao100.com>
2020-12-15 19:44:16 +0800

    feat: 添加统计课时

--- sql/统计课时.js
+++ sql/统计课时.js
@@ -1,13 +1,14 @@
 const a = [
   {
+    $match: {
+      teacherId: ''
+    },
+  },
+  {
     $group: {
       _id: { $dateToString: { format: '%Y-%m', date: '$createDate' } },
       key: { $first: { $month: '$createDate' } },
       count: { $sum: '$num' },
-    },
-  },
-  {
-    $match: {
     },
   }
 ]
