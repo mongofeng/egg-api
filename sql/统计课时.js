b09424d8b79b65f68eaa336e86e3f05e470fdca0
yangpu <yangpu@xiao100.com>
2020-12-15 20:31:46 +0800

    feat: 添加sql、

--- sql/统计课时.js
+++ sql/统计课时.js
@@ -12,3 +12,11 @@ const a = [
     },
   }
 ]
+
+
+const b = {
+  $match: {
+      teacherId: '5fc5fae45303720b576a750e',
+    $expr: { $gte: ['$createDate', { $toDate: '2020-12-15 00:00:00 +0800' }] }
+  }
+}
